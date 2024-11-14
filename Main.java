9c6f83665e0410c1b8fa866bff85fae21fccd8a9
Karlp-Web <karlenportillo1@gmail.com>
2024-11-14 18:48:14 +0000

    yomomma

--- Main.java
+++ Main.java
@@ -14,7 +14,23 @@ public class Main
 
   public static int countProperContains(String word, String target)
   {
-    return 0;
+    int count = 0;
+    int N = target.length();
+
+    for (int i = 0; i < word.length() - (N-1); i++ )
+    {
+      String sub = word.substring(i, i+N);
+      boolean beginOrSpaceBefore;
+      boolean endOrSpaceAfter;
+      int endOfTarget = i + 
+
+      boolean properlyContained = beginOrSpaceBefore && endOrSpaceAfter;
+      if (sub.equals(target) && properlyContained)
+      {
+        count++;
+      }
+    }
+    return count;
   }
 
   public static String deleteSub(String word, String target)
